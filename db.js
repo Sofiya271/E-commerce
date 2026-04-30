c9158cadab1a73155d95a4cfe362007b96369d5d
Sofiya <sofiyaqanbarli27@gmail.com>
2026-04-30 16:06:45 +0400

    Initial commit

---
+++ db.js
@@ -0,0 +1,19 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+  host: "127.0.0.1",
+  user: "root",
+  port: 3306,
+  password: "adminpass123",
+  database: "ecommerce_db"
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error("Database connection failed:", err);
+    return;
+  }
+  console.log("Connected to MySQL");
+});
+
+module.exports = connection;
---
+++ routes/categoryRoutes.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const router = express.Router();
+const categoryController = require("../controllers/categoryController");
+
+const authMiddleware = require('../middleware/authMiddleware');
+const roleMiddleware = require('../middleware/roleMiddleware');
+
+
+
+router.get("/", categoryController.getAllCategories);
+
+router.get("/:id", categoryController.getCategoryById);
+
+router.post("/", 
+authMiddleware,
+roleMiddleware(['admin']),
+categoryController.createCategory);
+
+router.put("/:id", authMiddleware,
+roleMiddleware(['admin']),
+categoryController.updateCategory);
+
+router.delete("/:id", authMiddleware,
+roleMiddleware(['admin']),
+categoryController.deleteCategory);
+
+module.exports = router;
